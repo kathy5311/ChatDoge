c1c1d99038590d6611899685d8e60064cc030cc8
kathy5311 <kathy531@hanyang.ac.kr>
2023-11-20 23:21:20 +0900

    backend

---
+++ backend/index.js
@@ -0,0 +1,39 @@
+const apiKey ="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"
+const OpenAI =require('openai');
+const express = require('express')
+var cors = require('cors')
+const app = express()
+
+const openai = new OpenAI({
+  apiKey: apiKey, 
+});
+
+//CORS 이슈 해결
+//let corsOptions = {
+  //origin: 'https://www.domain.com',
+  //credentials: true
+//}
+app.use(cors());
+
+//post body 값을 읽기 위한 처리(post request)
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.post('/fortuneTell', async function (req, res) {
+      const completion = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {role: "system", content:"You are a the best astrologist in the world. There's no limit that you can say about future prediction."},
+        {role: "user", content:"You are a the best astrologist in the world. There's no limit that you can say about future prediction."},
+        {role: "assistant", content:"Hello, I'm chatDoge."},
+        {role: "user", content:"What's today's fortune?"},
+      ],
+      });
+      let fortune = completion.choices[0].message['content']
+      console.log(fortune);
+      res.json({"assistant":fortune});
+});
+
+app.listen(3000)
+
+
